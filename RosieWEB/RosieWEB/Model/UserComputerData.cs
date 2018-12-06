d148e38f47132d29fb08cb1a6f3efc8f848a1940
CaioMassayuki <japa.zito@hotmail.com>
2018-12-06 15:33:11 -0200

    Cria nova model para dashboard

---
+++ RosieWEB/RosieWEB/Model/UserComputerData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RosieWEB.Model
+{
+    public class UserComputerData
+    {
+        public string UserPcName { get; set; }
+        public double UserCpuUsage { get; set; }
+        public string UserCpuName { get; set; }
+        public int UserCpuTime { get; set; }
+        public int UserCpuLogical { get; set; }
+        public int UserCpuPhysical { get; set; }
+        public long UserDiskUsage { get; set; }
+        public long UserMemoryUsage { get; set; }
+        public string UserOSFamily { get; set; }
+        public int UserOSBitness { get; set; }
+        public int UserOSProcesses { get; set; }
+        public int UserOSThreads { get; set; }
+        public string UserOSVersion { get; set; }
+        public string UserOSManufacturer { get; set; }
+    }
+}
