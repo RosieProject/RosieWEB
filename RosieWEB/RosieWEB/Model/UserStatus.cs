1fa9bdb6ae2e806bd522792fea367e6a08564a02
CaioMassayuki <japa.zito@hotmail.com>
2018-12-06 15:32:53 -0200

    Modifica model UserStatus

---
+++ RosieWEB/RosieWEB/Model/UserStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RosieWEB.Model
+{
+    public class UserStatus
+    {
+        public string userName { get; set; }
+        public double userCpu { get; set; }
+        public long userDisk { get; set; }
+        public long userMemory { get; set; }
+        public int userComputer { get; set; }
+    }
+}
