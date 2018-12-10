858640d1e3c9deebb5834f2fa02a7c24232c69a9
luizca95 <luizca95@hotmail.com>
2018-12-10 21:37:32 -0200

    Compara criptografia no login

--- RosieWEB/RosieWEB/Pages/Login.aspx.cs
+++ RosieWEB/RosieWEB/Pages/Login.aspx.cs
@@ -3,6 +3,8 @@ using System.Configuration;
 using System.Data.SqlClient;
 using System.Web;
 using System.Web.Services;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace RosieWEB.Pages
 {
@@ -10,6 +12,41 @@ namespace RosieWEB.Pages
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+
+        }
+
+
+        static string ComputeSha256Hash(string rawData)
+
+        {
+
+            // Create a SHA256 
+
+            using (SHA256 sha256Hash = SHA256.Create())
+
+            {
+
+                // ComputeHash - returns byte array 
+
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+
+                // Convert byte array to a string 
+
+                StringBuilder builder = new StringBuilder();
+
+                for (int i = 0; i < bytes.Length; i++)
+
+                {
+
+                    builder.Append(bytes[i].ToString("x2"));
+
+                }
+
+                return builder.ToString();
+
+            }
+
 
         }
 
@@ -21,8 +58,10 @@ namespace RosieWEB.Pages
 
             using (SqlConnection conn = new SqlConnection(strConn))
             {
+                string senhaCript = ComputeSha256Hash(senha);
+
                 conn.Open();
-                using (SqlCommand searchUser = new SqlCommand($"SELECT ID_Usuario, ID_Empresa, Nome_Usuario Email_Usuario, Senha_Usuario FROM Usuario WHERE Email_Usuario = '{email}' AND Senha_Usuario = '{senha}'", conn))
+                using (SqlCommand searchUser = new SqlCommand($"SELECT ID_Usuario, ID_Empresa, Nome_Usuario, Email_Usuario, Senha_Usuario FROM Usuario WHERE Email_Usuario = '{email}' AND Senha_Usuario = '{senhaCript}'", conn))
                 {
                     SqlDataReader rd = searchUser.ExecuteReader();
                     if (rd.Read())
@@ -33,11 +72,8 @@ namespace RosieWEB.Pages
 
                         return true;
                     }
-                    else
-                    {
-                        return false;
-                    }
                 }
+                return false;
             }
         }
     }
